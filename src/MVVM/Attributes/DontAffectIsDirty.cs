7a5cfde63f44314d472173d4ca40db613fba48f5
Jan M <j.morfeld@outlook.com>
2020-02-18 20:04:45 +0100

    changed attributes to be inherited because i see no reason to not do it

--- src/MVVM/Attributes/DontAffectIsDirty.cs
+++ src/MVVM/Attributes/DontAffectIsDirty.cs
@@ -5,7 +5,10 @@ namespace CodeMonkeys.MVVM.Attributes
     /// <summary>
     /// Prevents a property from changing the IsDirty flag (only affects classes inherited from ModelBase)
     /// </summary>
-    [AttributeUsage(AttributeTargets.Property)]
+    [AttributeUsage(
+        AttributeTargets.Property,
+        AllowMultiple = false,
+        Inherited = true)]
     public class DontAffectIsDirty :
         Attribute
     {
