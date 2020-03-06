fb6efcb9dc97604d25e621c1bc4aabc651cf680d
LegendaryB <daniel_belz@gmx.net>
2020-03-06 16:24:58 +0100

    Added file log extensions for the factory

---
+++ src/Logging/Logging.File/LogServiceFactory.File.Extensions.cs
@@ -0,0 +1,34 @@
+﻿using CodeMonkeys.Core;
+using CodeMonkeys.Core.Logging;
+
+using System;
+
+namespace CodeMonkeys.Logging.File
+{
+    public static partial class LogServiceFactoryExtensions
+    {
+        public static void AddFile(
+            this ILogServiceFactory _this,
+            FileLogOptions options)
+        {
+            Argument.NotNull(
+                options,
+                nameof(options));
+
+            var provider = new FileLogServiceProvider(options);
+
+            _this.AddProvider(provider);
+        }
+
+        public static void AddFile(
+            this ILogServiceFactory _this,
+            Func<FileLogOptions> optionsFactory)
+        {
+            Argument.NotNull(
+                optionsFactory,
+                nameof(optionsFactory));
+
+            _this.AddFile(optionsFactory());
+        }
+    }
+}
