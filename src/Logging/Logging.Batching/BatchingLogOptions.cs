f6adb19ea17c2f1619292541dc41c0edc164a876
LegendaryB <daniel_belz@gmx.net>
2020-03-06 16:12:45 +0100

    Added BatchingLogOptions and started to implement the base class

--- src/Logging/Logging.Batching/BatchingLogOptions.cs
+++ src/Logging/Logging.Batching/BatchingLogOptions.cs
@@ -1,9 +1,31 @@
 ﻿using CodeMonkeys.Logging.Configuration;
 
+using System;
+
 namespace CodeMonkeys.Logging.Batching
 {
     public class BatchingLogOptions : LogOptions
     {
-        
+        private TimeSpan _flushPeriod;
+        private int _batchSize = 50;
+        private int _queueSize = 1000;
+
+        public TimeSpan FlushPeriod
+        {
+            get => _flushPeriod;
+            set => SetValue(ref _flushPeriod, value);
+        }
+
+        public int BatchSize
+        {
+            get => _batchSize;
+            set => SetValue(ref _batchSize, value);
+        }
+
+        public int QueueSize
+        {
+            get => _queueSize;
+            set => SetValue(ref _queueSize, value);
+        }
     }
 }
--- src/Logging/Logging.Batching/BatchingLogServiceProvider.cs
+++ src/Logging/Logging.Batching/BatchingLogServiceProvider.cs
@@ -1,11 +1,16 @@
-﻿namespace CodeMonkeys.Logging.Batching
+﻿using System.Collections.Concurrent;
+
+namespace CodeMonkeys.Logging.Batching
 {
     public abstract class BatchingLogServiceProvider<TOptions> : LogServiceProvider<TOptions>
         where TOptions : BatchingLogOptions
     {
+        private readonly BlockingCollection<LogMessage> _queue;
+
         protected BatchingLogServiceProvider(TOptions options) 
             : base(options)
         {
+            _queue = new BlockingCollection<LogMessage>(options.QueueSize);
         }
     }
 }
