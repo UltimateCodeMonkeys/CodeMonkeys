708717825d0f51d01709c4c7e54bc50c040a19c0
LegendaryB <daniel_belz@gmx.net>
2020-03-12 11:16:39 +0100

    Added unit tests for the OptionsConsumer component / mechanism

---
+++ src/tests/UnitTests/Configuration/MockOptions.cs
@@ -0,0 +1,22 @@
+﻿using CodeMonkeys.Core.Configuration;
+
+namespace CodeMonkeys.UnitTests.Configuration
+{
+    public class MockOptions : Options
+    {
+        private bool _prop1;
+        private int _prop2;
+
+        public bool Prop1
+        {
+            get => _prop1;
+            set => SetValue(ref _prop1, value);
+        }
+
+        public int Prop2
+        {
+            get => _prop2;
+            set => SetValue(ref _prop2, value, false);
+        }
+    }
+}
