1485b2d6a325e3c64af7e8675995de5da5c8c7a6
LegendaryB <daniel_belz@gmx.net>
2020-04-14 12:18:32 +0200

    Current state with several changes

---
+++ src/tests/UnitTests/DependencyInjection/Container.cs
@@ -0,0 +1,71 @@
+﻿using CodeMonkeys.DependencyInjection;
+
+using System;
+
+namespace CodeMonkeys.UnitTests.DependencyInjection
+{
+    internal class Container :
+        DependencyContainer,
+        IDependencyContainer
+    {
+        internal override void SetContainer(object instance)
+        {
+            return;
+        }
+
+        public void RegisterInstance<TInstance>(TInstance instance) where TInstance : class
+        {
+            return;
+        }
+
+        public void RegisterInstance<TInstance>(Func<TInstance> factoryFunc) where TInstance : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RegisterSingleton(Type interfaceType, Type implementationType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RegisterSingleton<TImplementation>()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RegisterSingleton(Type typeToRegister)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RegisterType<TImplementation>()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RegisterType(Type typeToRegister)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RegisterType(Type interfaceType, Type implementationType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TInterfaceToResolve Resolve<TInterfaceToResolve>() where TInterfaceToResolve : class
+        {
+            throw new NotImplementedException();
+        }
+
+        void IDependencyRegister.RegisterSingleton<TInterface, TImplementation>()
+        {
+            throw new NotImplementedException();
+        }
+
+        void IDependencyRegister.RegisterType<TInterface, TImplementation>()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/tests/UnitTests/DependencyInjection/DependencyContainerFactoryTests.cs
@@ -0,0 +1,56 @@
+﻿using CodeMonkeys.DependencyInjection;
+using CodeMonkeys.Logging;
+
+using NUnit.Framework;
+
+using System;
+
+namespace CodeMonkeys.UnitTests.DependencyInjection
+{
+    [TestFixture]
+    public class DependencyContainerFactoryTests
+    {
+        [Test]
+        public void CreateInstance_InnerContainerParamIsPresent()
+        {
+            var instance = DependencyContainerFactory.CreateInstance<Container>(
+                 new Container());
+
+            Assert.NotNull(instance);
+        }
+
+        [Test]
+        public void CreateInstance_InnerContainerParamIsNull_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                DependencyContainerFactory.CreateInstance<Container>(
+                    null);
+            });
+        }
+
+        [Test]
+        public void CreateInstance_ContainerDoesNotImplementBaseClass_ThrowsNonDependencyContainerException()
+        {
+            Assert.Throws<NonDependencyContainerException>(() =>
+            {
+                DependencyContainerFactory.CreateInstance<BaselessContainer>(
+                    new BaselessContainer());
+            });
+        }
+
+        [Test]
+        public void CreateInstance_InnerContainerAndLogServiceParamIsPresent()
+        {
+            var logServiceFactory = new LogServiceFactory();
+            logServiceFactory.AddConsole();
+
+            var instance = DependencyContainerFactory.CreateInstance<Container>(
+                new Container(),
+                logServiceFactory.Create("unit test"));
+
+            Assert.NotNull(instance);
+            Assert.NotNull((instance as Container).Log);
+        }
+    }
+}
