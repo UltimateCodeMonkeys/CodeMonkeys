cd06a8776e35bc543aa93d43e0b4a6118b42c760
LegendaryB <daniel_belz@gmx.net>
2020-04-23 09:56:10 +0200

    Changed implementation of DryFactory to disable FastExpressionCompiler

--- src/DependencyInjection/DependencyInjection.DryIoC/DryFactory.cs
+++ src/DependencyInjection/DependencyInjection.DryIoC/DryFactory.cs
@@ -6,21 +6,32 @@ namespace CodeMonkeys.DependencyInjection.DryIoC
 {
     public static class DryFactory
     {
-        public static IDependencyContainer CreateInstance()
+        public static IDependencyContainer CreateInstance(
+            bool denyEmitUsage = false)
         {
             var instance = DependencyContainerFactory.CreateInstance<DryContainer>(
-                new Container());
+                CreateImplementationInstance(denyEmitUsage));
 
             return instance;
         }
 
         public static IDependencyContainer CreateInstance(
-            ILogService logService)
+            ILogService logService,
+            bool denyEmitUsage = false)
         {
             var instance = DependencyContainerFactory.CreateInstance<DryContainer>(
-                new Container(), logService);
+                CreateImplementationInstance(denyEmitUsage), 
+                logService);
 
             return instance;
+        }
+
+        private static Container CreateImplementationInstance(
+            bool denyEmitUsage)
+        {
+            return denyEmitUsage ?
+                new Container() :
+                new Container(r => r.WithoutFastExpressionCompiler());
         }
     }
 }
