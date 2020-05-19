e17b37f2b8c35981665cff31c3e00248407e3fa7
Jan M <j.morfeld@outlook.com>
2020-05-19 20:52:14 +0200

    initial commit, created service class

---
+++ src/Navigation/Navigation.WPF/NavigationHost.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace CodeMonkeys.Navigation.WPF
+{
+    public sealed class NavigationHost :
+        Frame
+    {
+        public static readonly DependencyProperty NavigationServiceProperty =
+            DependencyProperty.Register(
+                nameof(NavigationService),
+                typeof(INavigationService),
+                typeof(NavigationHost),
+                new PropertyMetadata(OnNavigationServiceChanged));
+
+        public new INavigationService NavigationService
+        {
+            get => (INavigationService)GetValue(NavigationServiceProperty);
+            set => SetValue(NavigationServiceProperty, value);
+        }
+
+        private static void OnNavigationServiceChanged(
+            DependencyObject @object,
+            DependencyPropertyChangedEventArgs eventArgs)
+        {
+            if (!(@object is NavigationHost navigationHost))
+                return;
+
+            if (!(eventArgs.NewValue is NavigationService navigationService))
+                return;
+            
+            
+
+        }
+    }
+}
---
+++ src/Navigation/Navigation.WPF/NavigationService.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CodeMonkeys.Navigation.WPF
+{
+    public class NavigationService :
+        INavigationService
+    {
+        public IReadOnlyCollection<INavigationRegistration> Registrations => throw new NotImplementedException();
+
+        public void ClearCache()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task CloseAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Register(INavigationRegistration registrationInfo)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task INavigationService.CloseAsync<TViewModelInterface>()
+        {
+            throw new NotImplementedException();
+        }
+
+        Task INavigationService.CloseAsync<TViewModelInterface, TParentViewModelInterface>()
+        {
+            throw new NotImplementedException();
+        }
+
+        Task INavigationService.CloseAsync<TViewModelInterface, TParentViewModelInterface, TResult>(TResult resultData)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task INavigationService.SetRoot<TViewModelInterface>()
+        {
+            throw new NotImplementedException();
+        }
+
+        Task INavigationService.ShowAsync<TViewModelInterface>()
+        {
+            throw new NotImplementedException();
+        }
+
+        Task INavigationService.ShowAsync<TViewModelInterface, TModel>(TModel modelToPass)
+        {
+            throw new NotImplementedException();
+        }
+
+        void INavigationService.Unregister<TViewModelInterface>()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
