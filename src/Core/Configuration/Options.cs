99c5478556814d057cd5221d6dcd6396f72685d2
LegendaryB <daniel_belz@gmx.net>
2020-05-26 15:47:10 +0200

    Created PropertyBag base class and implemented it in Options base class

--- src/Core/Configuration/Options.cs
+++ src/Core/Configuration/Options.cs
@@ -1,48 +1,23 @@
 ﻿using Microsoft.Extensions.Primitives;
-using System.Collections.Concurrent;
 using System.Runtime.CompilerServices;
 using System.Threading;
 
 namespace CodeMonkeys.Configuration
 {
-    public abstract class Options
+    public abstract class Options : PropertyBag
     {
-        private readonly ConcurrentDictionary<string, object> _properties =
-            new ConcurrentDictionary<string, object>();
-
         private OptionsChangeToken _token = new OptionsChangeToken();
 
         public IChangeToken GetChangeToken() => _token;
 
-        protected void SetValue<TProperty>(
-            TProperty value, 
-            bool reload = true,
+        protected void SetValueAndReload<TProperty>(
+            TProperty value,
             [CallerMemberName]string propertyName = "")
         {
-            // field = value;
+            SetValue(value, 
+                propertyName: propertyName);
 
-            if (reload)
-                Reload();
-        }
-
-        protected TProperty GetValue<TProperty>(
-            [CallerMemberName]string propertyName = "")
-        {
-            return (TProperty)_properties.GetOrAdd(
-                propertyName,
-                default(TProperty));
-        }
-
-        private bool SetPropertyValue(
-            string propertyName,
-            object value)
-        {
-            var stored = _properties.AddOrUpdate(
-                propertyName,
-                value,
-                (name, oldValue) => value);
-
-            return stored == value;
+            Reload();
         }
 
         private void Reload()
---
+++ src/Core/PropertyBag.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Concurrent;
+using System.Runtime.CompilerServices;
+
+namespace CodeMonkeys
+{
+    public abstract class PropertyBag
+    {
+        protected readonly ConcurrentDictionary<string, object> Properties =
+            new ConcurrentDictionary<string, object>();
+
+        protected virtual void SetValue<TProperty>(
+            TProperty value,
+            [CallerMemberName]string propertyName = "")
+        {
+            Properties.AddOrUpdate(
+                propertyName,
+                value,
+                (name, oldValue) => value);
+        }
+
+        protected virtual TProperty GetValue<TProperty>(
+            [CallerMemberName]string propertyName = "")
+        {
+            return (TProperty)Properties.GetOrAdd(
+                propertyName,
+                default(TProperty));
+        }
+    }
+}
