5e46406b982337d40e11aaed2db87c4f4245688a
LegendaryB <daniel_belz@gmx.net>
2020-05-28 15:13:49 +0200

    Added static Options field in OptionsConsumer and made adjustments on the Dialog Service

--- src/Core/Configuration/Options.cs
+++ src/Core/Configuration/Options.cs
@@ -1,25 +1,11 @@
-﻿using Microsoft.Extensions.Primitives;
-using System.Runtime.CompilerServices;
-using System.Threading;
+﻿using System.Runtime.CompilerServices;
 
 namespace CodeMonkeys.Configuration
 {
     public abstract class Options
     {
-        private OptionsChangeToken _token = new OptionsChangeToken();
-
         private readonly PropertyBag _propertyBag
             = new PropertyBag();
-
-        public IChangeToken GetChangeToken() => _token;
-
-        protected void SetValueAndReload<TProperty>(
-            TProperty value,
-            [CallerMemberName]string propertyName = "")
-        {
-            SetValue(value, propertyName);
-            Reload();
-        }
 
         protected bool SetValue<TProperty>(
             TProperty value,
@@ -35,12 +21,6 @@ namespace CodeMonkeys.Configuration
         {
             return _propertyBag.GetValue<TProperty>(
                 propertyName);
-        }
-
-        private void Reload()
-        {
-            var previousToken = Interlocked.Exchange(ref _token, new OptionsChangeToken());
-            previousToken.OnReload();
         }
     }
 }
--- src/Core/Configuration/OptionsConsumer.cs
+++ src/Core/Configuration/OptionsConsumer.cs
@@ -1,44 +1,9 @@
-﻿using Microsoft.Extensions.Primitives;
-
-using System;
-
-namespace CodeMonkeys.Configuration
+﻿namespace CodeMonkeys.Configuration
 {
-    public abstract class OptionsConsumer<TOptions> : IDisposable
-        where TOptions : Options
+    public abstract class OptionsConsumer<TOptions>
+        where TOptions : Options, new()
     {
-        private bool disposed = false;
-
-        protected IDisposable OptionsChangeToken;
-
-        protected OptionsConsumer(TOptions options)
-        {
-            OptionsChangeToken = ChangeToken.OnChange(
-                options.GetChangeToken,
-                OnOptionsChanged,
-                options);
-        }        
-
-        protected abstract void OnOptionsChanged(TOptions options);
-
-        #region IDisposable Support
-
-        protected virtual void Dispose(bool disposing)
-        {
-            if (!disposed)
-            {
-                if (disposing)
-                    OptionsChangeToken?.Dispose();
-
-                disposed = true;
-            }
-        }
-
-        public void Dispose()
-        {
-            Dispose(true);
-        }
-
-        #endregion
+        public static readonly TOptions Options =
+            new TOptions();
     }
 }
