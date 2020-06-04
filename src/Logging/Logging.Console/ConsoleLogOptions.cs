e6284878522f7106d24a105256e6741a4f7d8dec
LegendaryB <daniel_belz@gmx.net>
2020-06-04 17:59:18 +0200

    Added constants for the ConsoleLogOptions properties

--- src/Logging/Logging.Console/ConsoleLogOptions.cs
+++ src/Logging/Logging.Console/ConsoleLogOptions.cs
@@ -2,13 +2,15 @@
 {
     public class ConsoleLogOptions : LogOptions
     {
+        private const bool DEFAULT_COLORIZEOUTPUT = true;
+
         /// <summary>
         /// Flag which indicates if the console output should be colorized depending on the log level.
         /// <para>Default value: <see langword="true"/>.</para>
         /// </summary>
         public bool ColorizeOutput
         {
-            get => GetValue(true);
+            get => GetValue(DEFAULT_COLORIZEOUTPUT);
             set => SetValue(value);
         }
     }
