482f32c96590d68e3c889bde3dcf2333cc9c97fe
Pantheas <1674386+Pantheas@users.noreply.github.com>
2020-06-05 10:53:52 +0200

    added bindingbase options

---
+++ src/MVVM/PropertyChanged/BindingBase/BindingBaseOptions.cs
@@ -0,0 +1,20 @@
+﻿using CodeMonkeys.Configuration;
+
+namespace CodeMonkeys.MVVM.PropertyChanged
+{
+    public class BindingBaseOptions :
+        Options
+    {
+        public bool UseCommandRelevanceAttribute
+        {
+            get => GetValue<bool>();
+            set => SetValue(value);
+        }
+
+
+        public BindingBaseOptions()
+        {
+            UseCommandRelevanceAttribute = true;
+        }
+    }
+}
