ae38b6f8ca1408c7c3071c31018d3da48deaf064
LegendaryB <daniel_belz@gmx.net>
2020-06-24 23:09:07 +0200

    Added better logging extensions

---
+++ src/Core/Logging/Extensions/ILogServiceExtensions.Critical.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace CodeMonkeys.Logging
+{
+    public static partial class LogServiceExtensions
+    {
+        public static void Critical<TState>(
+            this ILogService service,
+            DateTimeOffset timestamp,
+            TState state,
+            Exception ex,
+            [CallerMemberName] string methodName = "")
+        {
+            service.Internal(
+                timestamp,
+                state,
+                ex,
+                methodName);
+        }
+
+        public static void Critical<TState>(
+            this ILogService service,
+            DateTimeOffset timestamp,
+            TState state,
+            [CallerMemberName] string methodName = "")
+        {
+            service.Internal(
+                timestamp,
+                state,
+                methodName);
+        }
+
+        public static void Critical(
+            this ILogService service,
+            DateTimeOffset timestamp,
+            Exception ex,
+            [CallerMemberName] string methodName = "")
+        {
+            service.Internal(
+                timestamp,
+                ex,
+                methodName);
+        }
+
+
+
+        public static void Critical<TState>(
+            this ILogService service,
+            TState message,
+            Exception ex,
+            [CallerMemberName] string methodName = "")
+        {
+            service.Internal(
+                message,
+                ex,
+                methodName);
+        }
+
+        public static void Critical<TState>(
+            this ILogService service,
+            TState message,
+            [CallerMemberName] string methodName = "")
+        {
+            service.Internal(
+                message,
+                methodName);
+        }
+
+        public static void Critical(
+            this ILogService service,
+            Exception ex,
+            [CallerMemberName] string methodName = "")
+        {
+            service.Internal(
+                ex,
+                methodName);
+        }
+
+
+
+        private static void Critical<TState>(
+            this ILogService service,
+            TState message,
+            Exception ex,
+            Func<TState, Exception, string> formatter,
+            [CallerMemberName] string methodName = "")
+        {
+            service.Internal(
+                message,
+                ex,
+                formatter,
+                methodName);
+        }
+
+        private static void Critical<TState>(
+            this ILogService service,
+            TState message,
+            Func<TState, Exception, string> formatter,
+            [CallerMemberName] string methodName = "")
+        {
+            service.Internal(
+                message,
+                formatter,
+                methodName);
+        }
+
+        private static void Critical(
+            this ILogService service,
+            Exception ex,
+            Func<object, Exception, string> formatter,
+            [CallerMemberName] string methodName = "")
+        {
+            service.Internal(
+                ex,
+                formatter,
+                methodName);
+        }
+
+
+
+        private static void Critical<TState>(
+            this ILogService service,
+            DateTimeOffset timestamp,
+            TState message,
+            Func<TState, Exception, string> formatter,
+            [CallerMemberName] string methodName = "")
+        {
+            service.Internal(
+                timestamp,
+                message,
+                formatter,
+                methodName);
+        }
+
+        private static void Critical(
+            this ILogService service,
+            DateTimeOffset timestamp,
+            Exception ex,
+            Func<object, Exception, string> formatter,
+            [CallerMemberName] string methodName = "")
+        {
+            service.Internal(
+                timestamp,
+                ex,
+                formatter,
+                methodName);
+        }
+
+        private static void Critical<TState>(
+            this ILogService service,
+            DateTimeOffset timestamp,
+            TState message,
+            Exception ex,
+            Func<TState, Exception, string> formatter,
+            [CallerMemberName] string methodName = "")
+        {
+            service.Internal(
+                timestamp,
+                message,
+                ex,
+                formatter,
+                methodName);
+        }
+    }
+}
---
+++ src/Core/Logging/Extensions/ILogServiceExtensions.Internal.cs
@@ -0,0 +1,264 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace CodeMonkeys.Logging
+{
+    public static partial class LogServiceExtensions
+    {
+        /// <summary>
+        /// Wrapper for <see cref="ILogServiceBase.Log{TState}(DateTimeOffset, LogLevel, TState, string)"./>
+        /// </summary>
+        public static void Internal<TState>(
+            this ILogService service,
+            DateTimeOffset timestamp,
+            TState state,
+            Exception ex,
+            string methodName = "",
+            [CallerMemberName] string extensionMethodName = "")
+        {
+            service.Internal(
+                timestamp,
+                state,
+                ex,
+                null,
+                methodName,
+                extensionMethodName);
+        }
+
+        /// <summary>
+        /// Wrapper for <see cref="ILogServiceBase.Log{TState}(DateTimeOffset, LogLevel, TState, string)"./>
+        /// </summary>
+        public static void Internal<TState>(
+            this ILogService service,
+            DateTimeOffset timestamp,
+            TState state,
+            string methodName = "",
+            [CallerMemberName] string extensionMethodName = "")
+        {
+            service.Internal(
+                timestamp,
+                state,
+                null,
+                null,
+                methodName,
+                extensionMethodName);
+        }
+
+        /// <summary>
+        /// Wrapper for <see cref="ILogServiceBase.Log{TState}(DateTimeOffset, LogLevel, TState, string)"./>
+        /// </summary>
+        public static void Internal(
+            this ILogService service,
+            DateTimeOffset timestamp,
+            Exception ex,
+            string methodName = "",
+            [CallerMemberName] string extensionMethodName = "")
+        {
+            service.Internal<object>(
+                timestamp,
+                null,
+                ex,
+                null,
+                methodName,
+                extensionMethodName);
+        }
+
+
+
+        /// <summary>
+        /// Wrapper for <see cref="ILogServiceBase.Log{TState}(LogLevel, TState, string)"./>
+        /// </summary>
+        public static void Internal<TState>(
+            this ILogService service,
+            TState message,
+            Exception ex,
+            string methodName = "",
+            [CallerMemberName] string extensionMethodName = "")
+        {
+            service.Internal(
+                DateTimeOffset.Now,
+                message,
+                ex,
+                null,
+                methodName,
+                extensionMethodName);
+        }
+
+        /// <summary>
+        /// Wrapper for <see cref="ILogServiceBase.Log{TState}(LogLevel, TState, string)"./>
+        /// </summary>
+        public static void Internal<TState>(
+            this ILogService service,
+            TState message,
+            string methodName = "",
+            [CallerMemberName] string extensionMethodName = "")
+        {
+            service.Internal(
+                DateTimeOffset.Now,
+                message,
+                null,
+                null,
+                methodName,
+                extensionMethodName);
+        }
+
+        /// <summary>
+        /// Wrapper for <see cref="ILogServiceBase.Log{TState}(LogLevel, TState, string)"./>
+        /// </summary>
+        public static void Internal(
+            this ILogService service,
+            Exception ex,
+            string methodName = "",
+            [CallerMemberName] string extensionMethodName = "")
+        {
+            service.Internal<object>(
+                DateTimeOffset.Now,
+                null,
+                ex,
+                null,
+                methodName,
+                extensionMethodName);
+        }
+
+
+
+        /// <summary>
+        /// Wrapper for <see cref="ILogServiceBase.Log{TState}(LogLevel, TState, Exception, Func{TState, Exception, string}, string)"./>
+        /// </summary>
+        private static void Internal<TState>(
+            this ILogService service,
+            TState message,
+            Exception ex,
+            Func<TState, Exception, string> formatter,
+            string methodName = "",
+            [CallerMemberName] string extensionMethodName = "")
+        {
+            service.Internal(
+                DateTimeOffset.Now,
+                message,
+                ex,
+                formatter,
+                methodName,
+                extensionMethodName);
+        }
+
+        /// <summary>
+        /// Wrapper for <see cref="ILogServiceBase.Log{TState}(LogLevel, TState, Exception, Func{TState, Exception, string}, string)"./>
+        /// </summary>
+        private static void Internal<TState>(
+            this ILogService service,
+            TState message,
+            Func<TState, Exception, string> formatter,
+            string methodName = "",
+            [CallerMemberName] string extensionMethodName = "")
+        {
+            service.Internal(
+                DateTimeOffset.Now,
+                message,
+                null,
+                formatter,
+                methodName,
+                extensionMethodName);
+        }
+
+        /// <summary>
+        /// Wrapper for <see cref="ILogServiceBase.Log{TState}(LogLevel, TState, Exception, Func{TState, Exception, string}, string)"./>
+        /// </summary>
+        private static void Internal(
+            this ILogService service,
+            Exception ex,
+            Func<object, Exception, string> formatter,
+            string methodName = "",
+            [CallerMemberName] string extensionMethodName = "")
+        {
+            service.Internal(
+                DateTimeOffset.Now,
+                null,
+                ex,
+                formatter,
+                methodName,
+                extensionMethodName);
+        }
+
+
+
+        /// <summary>
+        /// Wrapper for <see cref="ILogServiceBase.Log{TState}(DateTimeOffset, LogLevel, TState, Exception, Func{TState, Exception, string}, string)"./>
+        /// </summary>
+        private static void Internal<TState>(
+            this ILogService service,
+            DateTimeOffset timestamp,
+            TState message,
+            Func<TState, Exception, string> formatter,
+            string methodName = "",
+            [CallerMemberName] string extensionMethodName = "")
+        {
+            service.Internal(
+                timestamp,
+                message,
+                null,
+                formatter,
+                methodName,
+                extensionMethodName);
+        }
+
+        /// <summary>
+        /// Wrapper for <see cref="ILogServiceBase.Log{TState}(DateTimeOffset, LogLevel, TState, Exception, Func{TState, Exception, string}, string)"./>
+        /// </summary>
+        private static void Internal(
+            this ILogService service,
+            DateTimeOffset timestamp,
+            Exception ex,
+            Func<object, Exception, string> formatter,
+            string methodName = "",
+            [CallerMemberName] string extensionMethodName = "")
+        {
+            service.Internal(
+                timestamp,
+                null,
+                ex,
+                formatter,
+                methodName,
+                extensionMethodName);
+        }
+
+        /// <summary>
+        /// Wrapper for <see cref="ILogServiceBase.Log{TState}(DateTimeOffset, LogLevel, TState, Exception, Func{TState, Exception, string}, string)"./>
+        /// </summary>
+        private static void Internal<TState>(
+            this ILogService service,
+            DateTimeOffset timestamp,
+            TState message,
+            Exception ex,
+            Func<TState, Exception, string> formatter,
+            string methodName = "",
+            [CallerMemberName] string extensionMethodName = "")
+        {
+            if (!TryParseLogLevel(extensionMethodName, out var logLevel))
+                return;
+
+            service.Log(
+                timestamp,
+                logLevel,
+                message,
+                ex,
+                formatter,
+                methodName);
+        }
+
+        private static bool TryParseLogLevel(string input, out LogLevel logLevel)
+        {
+            logLevel = LogLevel.Trace;
+
+            if (string.IsNullOrWhiteSpace(input))
+                return false;
+
+            if (!Enum.TryParse<LogLevel>(input, out var level))
+                return false;
+
+            logLevel = level;
+
+            return true;
+        }
+    }
+}
