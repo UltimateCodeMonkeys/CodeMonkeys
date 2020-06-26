2270e722016d2b2555e6e64b1902a77c4781e6fd
LegendaryB <daniel_belz@gmx.net>
2020-06-27 00:44:52 +0200

    Added non generic methods

---
+++ src/tests/UnitTests/MVVM/ViewModelFactoryTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace CodeMonkeys.UnitTests.MVVM
+{
+    public class ViewModelFactoryTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+    }
+}
