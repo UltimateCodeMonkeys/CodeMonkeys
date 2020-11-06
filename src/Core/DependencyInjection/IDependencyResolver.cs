3bd2d15868286ff96c21535a259cf937d38af80d
Pantheas <1674386+Pantheas@users.noreply.github.com>
2020-11-06 21:47:49 +0100

    .

---
+++ src/Core/DependencyInjection/IDependencyResolver.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CodeMonkeys.DependencyInjection
+{
+    public interface IDependencyResolver
+    {
+        TInterfaceToResolve Resolve<TInterfaceToResolve>()
+            where TInterfaceToResolve : class;
+
+
+        object Resolve(Type interfaceType);
+
+        TImplementation Resolve<TImplementation>(Type interfaceType)
+            where TImplementation : class;
+    }
+}
