b78e7225ca8e71d9d0a80e5873d8adb5198bab13
LegendaryB <6391022+LegendaryB@users.noreply.github.com>
2021-04-06 14:46:32 +0200

    Added configure method to the service provider

--- src/Logging/Logging.AppCenter/ServiceProvider.cs
+++ src/Logging/Logging.AppCenter/ServiceProvider.cs
@@ -1,4 +1,6 @@
-﻿namespace CodeMonkeys.Logging.AppCenter
+﻿using System;
+
+namespace CodeMonkeys.Logging.AppCenter
 {
     public class ServiceProvider : ILogServiceProvider
     {
@@ -8,7 +10,29 @@
                 context,
                 nameof(context));
 
-            return new AppCenterLogService(context);
+            Configure();
+
+            return new AppCenterLogService(
+                context);
+        }
+
+        private void Configure()
+        {
+            var iosApiKey = AppCenterLogService.Options.IOSApiKey;
+            var androidApiKey = AppCenterLogService.Options.IOSApiKey;
+            var uwpApiKey = AppCenterLogService.Options.IOSApiKey;
+
+            if (string.IsNullOrWhiteSpace(iosApiKey) &&
+                string.IsNullOrWhiteSpace(androidApiKey) &&
+                string.IsNullOrWhiteSpace(uwpApiKey))
+            {
+                throw new ArgumentException("No api keys are configured for logging to Microsoft App Center!");
+            }
+
+            Microsoft.AppCenter.AppCenter.Configure(
+                $"ios={iosApiKey};" +
+                $"android={androidApiKey};" +
+                $"uwp={uwpApiKey}");
         }
     }
 }
